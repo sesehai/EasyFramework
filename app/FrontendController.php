23795c6d02648b8bdf99421cb9070da1fda5427b
sesehai <sesehai@gmail.com>
2011-08-01 15:01:59 +0800

    Edited app/FrontendController.php via GitHub

--- app/FrontendController.php
+++ app/FrontendController.php
@@ -1,28 +1,8 @@
 <?php
 class FrontendController extends BaseController {
 	protected $tplDir  = 'android/';
-	protected $pageTitle  = '乐视应用系统';
-	/**
-	 * 客户端产品类型配置:
-	 */
-	protected $_ptypeArr = array(
-		'iphone'=>'c1',
-		'ipad'=>'c2',
-		'androidphone'=>'c3',
-		'pc'=>'c4',
-		'sonypad'=>'c5',
-		'lepad'=>'c6',
-		'hisensetv'=>'c7',
-		'greatwallpad'=>'c8',
-		'3lepad'=>'c9',
-		'android21'=>'c10',
-		'epg'=>'c11',
-		'hipad'=>'c12',
-		'iphone21'=>'c13',
-		'letoupad'=>'c14',
-		'oppopad'=>'c15',
-		'xoompad'=>'c16',
-	);
+	protected $pageTitle  = '应用系统';
+	
 	public function __construct(){
 		parent::__construct();
 		$this->_init_view();
