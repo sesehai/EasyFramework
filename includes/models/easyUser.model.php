140e1360502f4e7aa2f0f7bc55b274da87c13ca6
sesehai <sesehai@gmail.com>
2011-08-01 15:03:25 +0800

    Edited includes/models/easyUser.model.php via GitHub

--- includes/models/easyUser.model.php
+++ includes/models/easyUser.model.php
@@ -2,42 +2,6 @@
 class EasyModel extends BaseModel {
 	public function __construct() {
 		parent::__construct();
-		
-		$privs = 'stat|test|view,default|default|default,default|default|wellcome';
-		$privs .= ',sinaweibo|weibouser|list,sinaweibo|weibouser|add,sinaweibo|weibouser|edit,sinaweibo|weibouser|delete';
-		$privs .= ',sinaweibo|usertype|list,sinaweibo|usertype|add,sinaweibo|usertype|edit,sinaweibo|usertype|delete';
-		$privs .= ',stat|statinfo|list,stat|statinfo|delete';
-		$privs .= ',stat|netstat|list,stat|netstat|delete';
-		$privs .= ',stat|ipadfeedback|list,stat|ipadfeedback|delete';
-		$privs .= ',stat|videostat|list,stat|videostat|delete';
-		$privs .= ',all';
-		
-		$privs_appstat01 = 'stat|info|view,default|default|default,default|default|wellcome';
-		
-		$letv007 = array(
-		'user_id'       => '1',
-		'passwd'        => '007',
-		'user_name'     => 'letv007',
-		'reg_time'      => '2011-06-02',
-		'last_login'    => '1',
-		'last_ip'       => '',
-		'privs'         => $privs,
-		);
-		
-		$appstat01 = array(
-		'user_id'       => '2',
-		'passwd'        => '001',
-		'user_name'     => 'letv007',
-		'reg_time'      => '2011-06-02',
-		'last_login'    => '1',
-		'last_ip'       => '',
-		'privs'         => $privs_appstat01,
-		);
-		
-		$this->users = array(
-			'letv007'=>$letv007,
-			'appstat01'=>$appstat01,
-		);
 	}
 
 	public function auth($user_name, $password){
